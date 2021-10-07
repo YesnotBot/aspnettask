4ccc1bf00962fac9a51ff5de1eebf2c3c51404b4
YesnotBot <52937832+YesnotBot@users.noreply.github.com>
2021-10-07 13:44:01 +0530

    Add files via upload

---
+++ dbo.PhoneBook.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[PhoneBook] (
+    [PhoneBookID] INT          IDENTITY (1, 1) NOT NULL,
+    [FirstName]   VARCHAR (50) NULL,
+    [LastName]    VARCHAR (50) NULL,
+    [Contact]     VARCHAR (50) NULL,
+    [Email]       VARCHAR (50) NULL,
+    PRIMARY KEY CLUSTERED ([PhoneBookID] ASC)
+);
+
